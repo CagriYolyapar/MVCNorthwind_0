e1caf0e78041cb5b2395c01400a51c4297208d8a
Cagri Yolyapar <cagri.yolyapar@gmail.com>
2022-04-12 14:05:18 +0300

    Add project files.

---
+++ MVCNorthwind_0/Controllers/CategoryController.cs
@@ -0,0 +1,40 @@
+﻿using MVCNorthwind_0.DesignPatterns.SingletonPattern;
+using MVCNorthwind_0.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCNorthwind_0.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        NorthwindEntities _db;
+
+        public CategoryController()
+        {
+            _db = DBTool.DBInstance;
+        }
+
+
+        public ActionResult BringCategories()
+        {
+            return View(_db.Categories.ToList());
+        }
+
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCategory(Category item)
+        {
+            _db.Categories.Add(item);
+            _db.SaveChanges();
+            return RedirectToAction("BringCategories");
+        }
+    }
+}
